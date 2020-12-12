7d2ab3b419b74319e52c43b7ed3ef3494333b20c
Purushottama Rao Dasari <74757654+Purush-IITMadras@users.noreply.github.com>
2020-12-12 20:49:24 +0530

    Real time simulation added with java script

--- main.js
+++ main.js
@@ -8,11 +8,6 @@ let max_time_input = document.getElementById('max-time');
 var slider1_value = 0;
 var slide2_value = 0;
 
-const red = 'rgb(255, 99, 132)'; // red
-const blue = 'rgb(54, 162, 235)'; // blue
-
-const chart1 = document.getElementById('box-2').getContext('2d');
-// var slider3_value = 0;
 
 min_time_input.oninput = function(){
     min_time_slider.value = this.value;
@@ -66,7 +61,8 @@ var myTimer;
     document.getElementById('stop-time-btn').style.display = "inline-block";
     myTimer = setInterval(myClock, 1000);
 
-    var c = parseInt(document.getElementById("start-time").value);
+    // var c = parseInt(document.getElementById("start-time").value);
+    var c = 1;
 
     var sliderVal = [];
     var totalVal = 0;
@@ -75,7 +71,7 @@ var myTimer;
         document.getElementById("start-time").value = c++;
         sliderVal.push(totalVal);
         // console.log(autoTime);
-        makechart(sliderVal, c);
+        // makechart(sliderVal, c);
     }
 // }
 
@@ -83,76 +79,34 @@ function stopClock() {
     document.getElementById('start-time-btn').style.display = "inline-block";
     document.getElementById('stop-time-btn').style.display = "none";
     clearInterval(myTimer);
+    clearInterval(clock);
 }
 
-// var autoStartTimer;
-// autoStartTimer = setInterval(autoClock, 1000);
-//     var c = parseInt(document.getElementById("auto-start-time").value);
 
-// function autoClock() {
-//     document.getElementById("auto-start-time").value = c++;   
-// }
+function getData() {
+    return document.getElementById('total-time').value;
+}  
 
+Plotly.plot('chart',[{
+    y:[getData()],
+    type:'line'
+}]);
 
-function makechart(slider3_value, autoTimer) { 
+var cnt = 0;
 
-    let entry = {
-        slider3_value: slider3_value,
-        autoTimer: autoTimer
+var clock = setInterval(function(){
+
+    Plotly.extendTraces('chart',{ y:[[getData()]]}, [0]);
+    cnt++;
+    if(cnt > 9) {
+        Plotly.relayout('chart',{
+            xaxis: {
+                range: [cnt-9,cnt]
+            }
+        });
     }
-    console.log(entry);
-    fetch(`${window.origin}/process-entry`, {
-        method: "POST",
-        body: JSON.stringify(entry),
-        cache: "no-cache",
-        headers: new Headers({
-            "content-type": "application/json",
-        })
-    })
-    .then(function (res) {
-        if (res.status !== 200) {
-            console.log(`Response is not 200: ${res.status}`);
-            return;
-        }
-        
-        res.json().then(function (val) {
-            chart = new Chart(chart1, {
-                type: 'line',
-                data: {
-                    labels: val.autoTimer,
-                    datasets: [{
-                        backgroundColor: red,
-                        borderColor: red,
-                        data: val.slider3_value,
-                        fill: false,
-                        label: 'Slider level'
-                    }]
-                },
-                options: {
-                    responsive: true,
-                    scales: {
-                        ticks: {
-                            stepSize: 15
-                        },
-                        yAxes: [{
-                            display: true,
-                            labelString: "Slider Value",
-                            ticks: {
-                                beginAtZero: true
-                            }
-                        }],
-                        xAxes: [{
-                            scaleLabel: {
-                                display: true,
-                                labelString: "Time(Mins)"
-                            }
-                        }]
-                    }
-                }
-            });
-        });   
-    });
-}   
+},1000);
+ 
 
 
 
